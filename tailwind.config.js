7633e2b8b89e2640d0b2785d5d814a4c1fef5470
Rupesh Sai Baba Chintakayala <47593513+rupeshchintakayala@users.noreply.github.com>
2025-08-28 15:46:26 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,8 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        'spin-wheel': 'spin-wheel 3s cubic-bezier(0.23, 1, 0.32, 1) forwards',
-      },
-      keyframes: {
-        'spin-wheel': {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(1800deg)' },
-        }
+      transitionTimingFunction: {
+        'wheel': 'cubic-bezier(0.23, 1, 0.32, 1)',
       }
     },
   },
